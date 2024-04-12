82391de00be655ff8ce91ac09667db02f851480b
AnatoliyBakshaev <nuklimzy@gmail.com>
2024-04-12 16:42:16 +0300

    е

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,38 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template<typename T, int Size>
 class TStack {
-  // добавьте код стека
+ private:
+    T data[Size];
+    int top;
+ public:
+    TStack() : top(0) {}
+
+    void push(const T &item) {
+        if (top <= Size - 1)
+            data[top++] = item;
+        else
+            throw std::string("Full!");
+    }
+
+    T pop() {
+        if (top > 0)
+            return data[--top];
+        else
+            throw std::string("Empty!");
+    }
+
+    bool isEmpty() {
+        return top == 0;
+    }
+
+    T get() {
+        if (!isEmpty())
+            return data[top - 1];
+        else
+            throw std::string("is Empty");
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
